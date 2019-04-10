caa734e66989f8b5d96867edc71e695c907389dd
tguerin <t_guerin@hotmail.com>
2019-04-10 11:27:48 +0200

    fix(K2xForm): text shouldn't be editable

--- Keyboard2XinputGui/K2xForm.Designer.cs
+++ Keyboard2XinputGui/K2xForm.Designer.cs
@@ -37,6 +37,7 @@
             // 
             this.richTextBox1.Location = new System.Drawing.Point(12, 12);
             this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
             this.richTextBox1.Size = new System.Drawing.Size(480, 56);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
